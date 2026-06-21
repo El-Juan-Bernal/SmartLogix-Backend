f360f08f63891408ad682ff3d216ccc796c4a391
El-Juan-Bernal <ju.bernala@duocuc.cl>
2026-06-21 03:04:10 -0400

    fix(dockerfile):ajustes en el dockerfile

--- ms_usuario/src/main/java/com/jaba/ms_usuario/config/RabbitMQConfig.java
+++ ms_usuario/src/main/java/com/jaba/ms_usuario/config/RabbitMQConfig.java
@@ -1,32 +1,36 @@
 package com.jaba.ms_usuario.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    // Nombres estandarizados
     public static final String EXCHANGE_NAME = "smartlogix.exchange";
     public static final String QUEUE_USUARIO = "smartlogix.usuario.queue";
     public static final String QUEUE_USUARIO_DLQ = "smartlogix.usuario.dlq";
     public static final String ROUTING_KEY_USUARIO = "usuario.creado";
     public static final String ROUTING_KEY_USUARIO_DLQ = "usuario.dlq";
 
-    // 1. El agrupador central (Exchange)
+    // --- EL TRADUCTOR JSON (¡Lo que faltaba!) ---
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
     @Bean
     public DirectExchange smartlogixExchange() {
         return new DirectExchange(EXCHANGE_NAME);
     }
 
-    // 2. La cola de fallos (DLQ)
     @Bean
     public Queue usuarioDlq() {
         return QueueBuilder.durable(QUEUE_USUARIO_DLQ).build();
     }
 
-    // 3. La cola principal (Con instrucciones de rebote hacia la DLQ)
     @Bean
     public Queue usuarioQueue() {
         return QueueBuilder.durable(QUEUE_USUARIO)
@@ -35,13 +39,11 @@ public class RabbitMQConfig {
                 .build();
     }
 
-    // 4. Enlace: Conectar la DLQ al Exchange
     @Bean
     public Binding dlqBinding() {
         return BindingBuilder.bind(usuarioDlq()).to(smartlogixExchange()).with(ROUTING_KEY_USUARIO_DLQ);
     }
 
-    // 5. Enlace: Conectar la Cola Principal al Exchange
     @Bean
     public Binding usuarioBinding() {
         return BindingBuilder.bind(usuarioQueue()).to(smartlogixExchange()).with(ROUTING_KEY_USUARIO);
